7194d5acbbf701b502e864c5d0b4d47fb741e122
Reissimpson <Reissimpson@users.noreply.github.com>
2026-04-29 21:56:02 -0400

    Configure AppSheet embed URL

--- site/config.js
+++ site/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  appsheetUrl: "https://www.appsheet.com/start/YOUR_APP_ID",
+  appsheetUrl: "https://www.appsheet.com/start/9e8c8ef1-3f9b-477e-bcb9-d14dbcb99557",
   title: "FRC Worlds Hot Matches",
   subtitle: "Live match watch list powered by AppSheet"
 };
